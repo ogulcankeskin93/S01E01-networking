a7f9e623c3f5a1d37e5787adbe32fcfd689b79e3
ogulcan keskin <ogulcankeskin93@gmail.com>
2019-08-10 18:40:35 +0300

    Swift 5 refactor

--- Networking.playground/Contents.swift
+++ Networking.playground/Contents.swift
@@ -5,12 +5,12 @@
 //: It will serve up the current directory, so make sure to be in the directory containing episodes.json
 
 import UIKit
-import XCPlayground
+import PlaygroundSupport
 
 
 typealias JSONDictionary = [String: AnyObject]
 
-let url = NSURL(string: "http://localhost:8000/episodes.json")!
+let url = NSURL(string: "http://localhost:80/episodes.json")!
 
 
 struct Episode {
@@ -21,7 +21,7 @@ struct Episode {
 extension Episode {
     init?(dictionary: JSONDictionary) {
         guard let id = dictionary["id"] as? String,
-            title = dictionary["title"] as? String else { return nil }
+            let title = dictionary["title"] as? String else { return nil }
         self.id = id
         self.title = title
     }
@@ -33,14 +33,14 @@ struct Media {}
 
 struct Resource<A> {
     let url: NSURL
-    let parse: NSData -> A?
+    let parse: (Data) -> A?
 }
 
 extension Resource {
-    init(url: NSURL, parseJSON: AnyObject -> A?) {
+    init(url: NSURL, parseJSON: @escaping (Any) -> A?) {
         self.url = url
         self.parse = { data in
-            let json = try? NSJSONSerialization.JSONObjectWithData(data, options: [])
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
             return json.flatMap(parseJSON)
         }
     }
@@ -50,26 +50,30 @@ extension Resource {
 extension Episode {
     static let all = Resource<[Episode]>(url: url, parseJSON: { json in
         guard let dictionaries = json as? [JSONDictionary] else { return nil }
-        return dictionaries.flatMap(Episode.init)
+        return dictionaries.compactMap(Episode.init)
     })
 }
 
 
 final class Webservice {
-    func load<A>(resource: Resource<A>, completion: (A?) -> ()) {
-        NSURLSession.sharedSession().dataTaskWithURL(resource.url) { data, _, _ in
+    func load<A>(resource: Resource<A>, completion: @escaping (A?) -> ()) {
+        URLSession.shared.dataTask(with: resource.url as URL) { data, _, _ in
             guard let data = data else {
                 completion(nil)
                 return
             }
             completion(resource.parse(data))
-        }.resume()
+            }.resume()
     }
 }
 
 
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-Webservice().load(Episode.all) { result in
-    print(result)
+Webservice().load(resource: Episode.all) { result in
+    guard let result = result else {return}
+    result.forEach {
+        print($0.id)
+        print($0.title)
+    }
 }
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 ## Networking
 
 This is the code that accompanies Swift Talk Episode 1: [Networking](https://talk.objc.io/episodes/S01E01-networking)
+
+Refactor for Swift 5. 
+
+    to start a apache web server from your mac, run “ sudo apachectl start “ and your htdocs is in /Library/WebService/
+    
+    the link should be http://localhost:80/episodes.json
